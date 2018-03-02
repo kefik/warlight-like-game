b3bafa4bea2b94b3f48c9d12aef19335c4ce6303
Jan Palášek <jan.palasek@seznam.cz>
2018-03-02 20:58:40 +0100

    #25 - added tree collection

---
+++ Common.Tests/Collections/TreeTests.cs
@@ -0,0 +1,98 @@
+﻿namespace Common.Tests.Collections
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Common.Collections;
+    using NUnit.Framework;
+
+    using static NUnit.Framework.Assert;
+
+    internal class TestTreeNode : TreeNode<TestTreeNode, char>
+    {
+        
+    }
+
+    [TestFixture]
+    public class TreeTests
+    {
+        private Tree<TestTreeNode, char> tree;
+
+        [SetUp]
+        public void Initialize()
+        {
+            tree = new Tree<TestTreeNode, char>();
+
+            var b = new TestTreeNode();
+
+            tree.Root = b;
+
+            b.Value = 'B';
+
+            var a = new TestTreeNode()
+            {
+                Parent = b,
+                Value = 'A'
+            };
+            var d = new TestTreeNode()
+            {
+                Parent = b,
+                Value = 'D'
+            };
+
+            b.Children = new List<TestTreeNode>()
+            {
+                a, d
+            };
+
+            var c = new TestTreeNode()
+            {
+                Parent = d,
+                Value = 'C'
+            };
+
+            var e = new TestTreeNode()
+            {
+                Parent = d,
+                Value = 'E'
+            };
+
+            d.Children = new List<TestTreeNode>()
+            {
+                c, e
+            };
+        }
+
+        [Test]
+        public void ForEachPostOrderTest()
+        {
+            var sb = new StringBuilder();
+
+            tree.ForEachPostOrder(x => sb.Append($"{x.Value} "));
+
+            AreEqual("A C E D B ", sb.ToString());
+        }
+
+        [Test]
+        public void ForEachPreOrderTest()
+        {
+            var sb = new StringBuilder();
+
+            tree.ForEachPreOrder(x => sb.Append($"{x.Value} "));
+
+            AreEqual("B A D C E ", sb.ToString());
+        }
+
+        [Test]
+        public void AddChildTest()
+        {
+            var d = tree.Root.Children[1];
+            d.AddChild('F');
+
+            var f = d.Children.Last();
+
+            AreSame(f.Parent, d);
+            AreEqual('F', f.Value);
+        }
+    }
+}
---
+++ Common/Collections/Tree.cs
@@ -0,0 +1,111 @@
+﻿namespace Common.Collections
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Represents a general tree structure with n children at each level.
+    /// </summary>
+    /// <typeparam name="TValue">Type of the node value.</typeparam>
+    /// <typeparam name="TNodeType"></typeparam>
+    public class Tree<TNodeType, TValue> where TNodeType : TreeNode<TNodeType, TValue>, new()
+    {
+        /// <summary>
+        /// Root of the tree. It's parent is null.
+        /// </summary>
+        public TNodeType Root { get; set; }
+
+        /// <summary>
+        /// Traverses the tree post-order and does specified action on each tree node.
+        /// The specified action is invoked after every child node is visited.
+        /// </summary>
+        /// <param name="action">Action to be invoked on every node.</param>
+        public void ForEachPostOrder(Action<TNodeType> action)
+        {
+            ForEachPostOrder(Root, action);
+        }
+
+        private void ForEachPostOrder(TNodeType currentNode, Action<TNodeType> action)
+        {
+            if (!currentNode.IsLeaf)
+            {
+                foreach (var currentNodeChild in currentNode.Children)
+                {
+                    ForEachPostOrder(currentNodeChild, action);
+                }
+            }
+
+            action(currentNode);
+        }
+
+        /// <summary>
+        /// Traverses the tree pre-order and does specified action on each tree node.
+        /// The specified action is invoked after every child node is visited.
+        /// </summary>
+        /// <param name="action">Action to be invoked on every node.</param>
+        public void ForEachPreOrder(Action<TNodeType> action)
+        {
+            ForEachPreOrder(Root, action);
+        }
+
+        private void ForEachPreOrder(TNodeType currentNode, Action<TNodeType> action)
+        {
+            // save children (handling situation when action resets currentNode's children)
+            var children = currentNode.Children;
+
+            action(currentNode);
+
+            if (!currentNode.IsLeaf)
+            {
+                foreach (var currentNodeChild in children)
+                {
+                    ForEachPreOrder(currentNodeChild, action);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Represents a tree node.
+    /// </summary>
+    /// <typeparam name="TValue">Value that the node contains.</typeparam>
+    /// <typeparam name="TNodeType">Type of the node.</typeparam>
+    public abstract class TreeNode<TNodeType, TValue> where TNodeType : TreeNode<TNodeType, TValue>, new()
+    {
+        public TNodeType Parent { get; set; }
+        public List<TNodeType> Children { get; set; }
+
+        public TValue Value { get; set; }
+
+        public bool IsLeaf
+        {
+            get { return Children == null || Children.Count == 0; }
+        }
+
+        public bool IsRoot
+        {
+            get { return Parent == null; }
+        }
+
+        public TNodeType GetChild(int index)
+        {
+            return Children[index];
+        }
+
+        public virtual void AddChild(TValue value)
+        {
+            var node = new TNodeType()
+            {
+                Value = value,
+                Parent = (TNodeType)this
+            };
+
+            Children.Add(node);
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
